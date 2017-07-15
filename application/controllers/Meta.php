00c2b65cbe91260193c24be46720a05236240fe0
Joel Kennedy <joelkennedy@sky.com>
2017-07-15 19:49:09 +0100

    Add Meta endpoint

---
+++ application/controllers/Meta.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Meta extends CI_Controller {
+
+	public function index()
+	{
+		$response = array(
+			'id' => 'scaleway',
+			'name' => 'Scaleway',
+			'server_nick_name' => 'Server',
+			'default_region' => 'par1',
+			'default_size' => '4GB',
+			'default_plan' => 'VC1S',
+			'can_reboot' => true,
+			'can_rename' => true,
+			'internal_iface' => 'eth1',
+			'external_iface' => 'eth0',
+			'ssh_user' => 'root',
+			'ssh_auth_method' => 'key',
+			'ssh_key_method' => 'reference',
+			'bootstrap_script' => 'https://s3.amazonaws.com/tools.nanobox.io/bootstrap/ubuntu.sh',
+			'credentials_fields' => array(
+				array('key' => 'token', 'label' => 'Access Token')
+			),
+			'instructions' => 'For instructions on how to retrieve a Scaleway access token, visit the repository.'
+		);
+
+		$this->output
+        ->set_status_header(200)
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+        ->_display();
+		exit;
+	}
+}
