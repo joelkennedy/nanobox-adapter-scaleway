acf3eded4576b4e3b0844200d9af4ef3d2f6f1c9
Joel Kennedy <joelkennedy@sky.com>
2017-07-16 01:09:48 +0100

    Update Catalog to match spec

--- application/controllers/Catalog.php
+++ application/controllers/Catalog.php
@@ -7,20 +7,24 @@ class Catalog extends REST_Controller {
 	public function index_get()
 	{
 		$response = array(
-			'id' => 'par1',
-			'name' => 'Paris',
-			'plans' => array(
-				array(
-					'id' => 'VC1S',
-					'name' => 'Cloud Server',
-					'specs' => array(
-						'id' => "VC1S",
-						'ram' => 2048,
-						'cpu' => 2,
-						'disk' => 50,
-						'transfer' => 'Unlimited',
-						'dollars_per_hr' => '0.006',
-						'dollars_per_mo' => '2.99'
+			array(
+				'id' => 'par1',
+				'name' => 'Paris',
+				'plans' => array(
+					array(
+						'id' => 'VC1S',
+						'name' => 'Cloud Server',
+						'specs' => array(
+							array(
+								'id' => "VC1S",
+								'ram' => 2048,
+								'cpu' => 2,
+								'disk' => 50,
+								'transfer' => 'Unlimited',
+								'dollars_per_hr' => '0.006',
+								'dollars_per_mo' => '2.99'
+							)
+						)
 					)
 				)
 			)
