29fdbe094ee91dba42b8ed1c4ee86735b5eab41c
Joel Kennedy <joelkennedy@sky.com>
2017-07-16 01:04:08 +0100

    Fix incorrect key in Meta data

--- application/controllers/Meta.php
+++ application/controllers/Meta.php
@@ -21,7 +21,7 @@ class Meta extends REST_Controller {
 			'ssh_auth_method' => 'key',
 			'ssh_key_method' => 'reference',
 			'bootstrap_script' => 'https://s3.amazonaws.com/tools.nanobox.io/bootstrap/ubuntu.sh',
-			'credentials_fields' => array(
+			'credential_fields' => array(
 				array('key' => 'access-token', 'label' => 'Auth Token')
 			),
 			'instructions' => 'For instructions on how to retrieve a Scaleway access token, visit the repository.'
