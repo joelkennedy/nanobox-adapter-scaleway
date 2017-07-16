f8d259de29986b6e9905787fc96ed7806496c8a7
Joel Kennedy <joelkennedy@sky.com>
2017-07-16 17:44:54 +0100

    Include Baremetal and Workload Intensive servers in the catalog

--- application/controllers/Catalog.php
+++ application/controllers/Catalog.php
@@ -43,6 +43,81 @@ class Catalog extends REST_Controller {
 								'dollars_per_mo' => 9.99
 							)
 						)
+					),
+					array(
+						'id' => 'baremetal',
+						'name' => 'Baremetal Server',
+						'specs' => array(
+							array(
+								'id' => "C2S",
+								'ram' => 8192,
+								'cpu' => 4,
+								'disk' => 50,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.024,
+								'dollars_per_mo' => 11.99
+							),
+							array(
+								'id' => "C2M",
+								'ram' => 16384,
+								'cpu' => 8,
+								'disk' => 50,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.036,
+								'dollars_per_mo' => 17.99
+							),
+							array(
+								'id' => "C2L",
+								'ram' => 32768,
+								'cpu' => 8,
+								'disk' => 50,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.048,
+								'dollars_per_mo' => 23.99
+							)
+						)
+					),
+					array(
+						'id' => 'workloadintensive',
+						'name' => 'Workload Intensive',
+						'specs' => array(
+							array(
+								'id' => "X64-15GB",
+								'ram' => 15360,
+								'cpu' => 6,
+								'disk' => 200,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.050,
+								'dollars_per_mo' => 24.99
+							),
+							array(
+								'id' => "X64-30GB",
+								'ram' => 30720,
+								'cpu' => 8,
+								'disk' => 400,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.100,
+								'dollars_per_mo' => 49.99
+							),
+							array(
+								'id' => "X64-60GB",
+								'ram' => 61440,
+								'cpu' => 10,
+								'disk' => 700,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.180,
+								'dollars_per_mo' => 89.99
+							),
+							array(
+								'id' => "X64-120GB",
+								'ram' => 122880,
+								'cpu' => 12,
+								'disk' => 1000,
+								'transfer' => "Unlimited",
+								'dollars_per_hr' => 0.360,
+								'dollars_per_mo' => 179.99
+							)
+						)
 					)
 				)
 			)
